8365e1a0afba8f9b266a6c5ed91b000dcf6823e8
X1ag <super.don17080@gmail.com>
2025-02-12 10:06:55 +0300

    new fixs

--- scripts/deployJettonTester.ts
+++ scripts/deployJettonTester.ts
@@ -5,16 +5,16 @@ import { jettonContentToCell, jettonMinterConfigToCell } from '../wrappers/Jetto
 import { JettonWallet, jettonWalletConfigToCell } from '../wrappers/JettonWallet'
 
 export async function run(provider: NetworkProvider) {
-    const jmc = await compile('JettonMinter')
-    const jwc = await compile('JettonWallet')
-    const content = jettonContentToCell({type:1, uri:"https://raw.githubusercontent.com/X1ag/dywe-manifest/refs/heads/main/jettontester.json?token=GHSAT0AAAAAACWRHBR5C7FITPWU7PGR45EOZ5HONKA"}) // I GUESS IT IS NOT WORKING BECAUSE I POST BAD DATA. CHECK THIS PLS 
+    const jetton_minter_code = await compile('JettonMinter')
+    const jetton_wallet_code = await compile('JettonWallet')
+    const content = jettonContentToCell({type:1, uri:"https://raw.githubusercontent.com/X1ag/dywe-manifest/refs/heads/main/jettontester.json?token=GHSAT0AAAAAACWRHBR42XSE2SR46QOGOZ2CZ5MFDZQ"}) // I GUESS IT IS NOT WORKING BECAUSE I POST BAD DATA. CHECK THIS PLS 
     const minter_data = jettonMinterConfigToCell({
             admin: address("UQCCUre08Opa3u2OkALTHcb0dboE-FUnkyoOqJVTO1h5HCKF"), 
             content: content,
-            wallet_code: jwc
+            wallet_code: jetton_wallet_code
         });
     
-    const jettonTester = provider.open(JettonTester.createFromConfig({jmc:jmc, jwc:jwc, data:minter_data}, await compile('JettonTester')));
+    const jettonTester = provider.open(JettonTester.createFromConfig({jmc:jetton_minter_code, jwc:jetton_wallet_code, data:minter_data}, await compile('JettonTester')));
 
     await jettonTester.sendDeploy(provider.sender(), toNano('0.05'));
 
